2534466f036fa2c6b945452a5a2aef1c389188d3
Christian Yao Komenan <kycdev@gmail.com>
2018-08-21 16:03:48 +0000

    commit before trying to upgrade swarm

--- datastructure-client-application/src/main/java/org/cyk/system/datastructure/client/application/collection/set/nestedset/NestedSetEditPage.java
+++ datastructure-client-application/src/main/java/org/cyk/system/datastructure/client/application/collection/set/nestedset/NestedSetEditPage.java
@@ -4,29 +4,22 @@ import java.io.Serializable;
 
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
-import javax.ws.rs.core.UriBuilder;
 
+import org.cyk.jee.utility.client.controller.web.jsf.primefaces.AbstractPageImpl;
 import org.cyk.system.datastructure.server.representation.api.collection.set.nested.NestedSetRepresentation;
 import org.cyk.system.datastructure.server.representation.entities.collection.set.nested.NestedSetDto;
-import org.cyk.utility.__kernel__.DependencyInjection;
-import org.cyk.utility.instance.InstanceHelper;
-import org.jboss.resteasy.client.jaxrs.ResteasyClient;
-import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
-import org.jboss.resteasy.client.jaxrs.ResteasyWebTarget;
 
 import lombok.Getter;
 import lombok.Setter;
 
 @Named @ViewScoped @Getter @Setter
-public class NestedSetEditPage implements Serializable {
+public class NestedSetEditPage extends AbstractPageImpl implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	private NestedSet nestedSet = new NestedSet();
 		
 	public void create() {
-		ResteasyClient client = new ResteasyClientBuilder().build();
-		ResteasyWebTarget target = client.target(UriBuilder.fromPath("http://localhost:8081"));
-		NestedSetRepresentation nestedSetRepresentation = target.proxy(NestedSetRepresentation.class);
-		nestedSetRepresentation.createOne(DependencyInjection.inject(InstanceHelper.class).buildOne(NestedSetDto.class, nestedSet));
+		NestedSetRepresentation nestedSetRepresentation = __getProxy__(NestedSetRepresentation.class);
+		nestedSetRepresentation.createOne(__injectInstanceHelper__().buildOne(NestedSetDto.class, nestedSet));
 	}
 }
--- datastructure-client-application/src/main/java/org/cyk/system/datastructure/client/application/collection/set/nestedset/NestedSetListPage.java
+++ datastructure-client-application/src/main/java/org/cyk/system/datastructure/client/application/collection/set/nestedset/NestedSetListPage.java
@@ -6,31 +6,24 @@ import java.util.List;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Named;
-import javax.ws.rs.core.GenericType;
-import javax.ws.rs.core.UriBuilder;
+import javax.ws.rs.core.Response;
 
+import org.cyk.jee.utility.client.controller.web.jsf.primefaces.AbstractPageImpl;
 import org.cyk.system.datastructure.server.representation.api.collection.set.nested.NestedSetRepresentation;
 import org.cyk.system.datastructure.server.representation.entities.collection.set.nested.NestedSetDto;
-import org.cyk.utility.__kernel__.DependencyInjection;
-import org.cyk.utility.instance.InstanceHelper;
-import org.jboss.resteasy.client.jaxrs.ResteasyClient;
-import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
-import org.jboss.resteasy.client.jaxrs.ResteasyWebTarget;
 
 @Named @RequestScoped
-public class NestedSetListPage implements Serializable {
+public class NestedSetListPage extends AbstractPageImpl implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	private List<NestedSet> nestedSets;
 	
 	public List<NestedSet> getNestedSets() {
 		if(nestedSets == null){
-			ResteasyClient client = new ResteasyClientBuilder().build();
-			ResteasyWebTarget target = client.target(UriBuilder.fromPath("http://localhost:8081"));
-			
-			NestedSetRepresentation nestedSetRepresentation = target.proxy(NestedSetRepresentation.class);
-			Collection<NestedSetDto> dtos = nestedSetRepresentation.getMany().readEntity(new GenericType<Collection<NestedSetDto>>(){});
-			nestedSets = (List<NestedSet>) DependencyInjection.inject(InstanceHelper.class).buildMany(NestedSet.class, dtos);
+			NestedSetRepresentation nestedSetRepresentation = __getProxy__(NestedSetRepresentation.class);
+			Response response = nestedSetRepresentation.getMany();
+			Collection<NestedSetDto> dtos = __readEntityAsCollection__(response, NestedSetDto.class);
+			nestedSets = (List<NestedSet>) __injectInstanceHelper__().buildMany(NestedSet.class, dtos);
 		}
 		return nestedSets;
 	}
